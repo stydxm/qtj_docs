5db8f2c2df8282d8bbcd4c3440d3f72d088d285e
stydxm <stydxm@outlook.com>
2024-08-18 21:34:26 +0800

    添加二维码组件

---
+++ .vitepress/theme/components/Qrcode.vue
@@ -0,0 +1,13 @@
+<script setup>
+import VueQrcode from '@chenfengyuan/vue-qrcode'
+import { defineProps } from "vue"
+const { content } = defineProps({
+    content: {
+        type: String,
+        require: true
+    }
+})
+</script>
+<template>
+    <vue-qrcode :value='`${content}`'></vue-qrcode>
+</template>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,5 +1,12 @@
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import "@fontsource/jetbrains-mono"
 import './custom-font.css'
+import Qrcode from './components/Qrcode.vue'
 
-export default DefaultTheme
+// export default DefaultTheme
+export default {
+    extends: DefaultTheme,
+    enhanceApp({ app }) {
+        app.component('Qrcode', Qrcode);
+    }
+}
