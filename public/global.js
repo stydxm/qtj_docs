00a062d4490da38d437aec1ff2b5eb4c0c9a6064
stydxm <stydxm@outlook.com>
2024-08-24 08:19:33 +0800

    主页不弹提示窗口

--- public/global.js
+++ public/global.js
@@ -15,7 +15,9 @@ function detectMobile() {
     return ua.includes("mobi") || ua.includes("android") || ua.includes("iphone")
 }
 
+const dir = window.location.href.toString().split(window.location.host)[1]
 const browser = detectBrowser()
 const mobile = detectMobile()
-if (browser) alert(`请不要使用${browser}访问\n建议使用电脑端的chrome或firefox或edge`)
-else if (mobile) alert("建议使用电脑访问")
+if (dir !== "/")
+    if (browser) alert(`请不要使用${browser}访问\n建议使用电脑端的chrome或firefox或edge`)
+    else if (mobile) alert("建议使用电脑访问")
