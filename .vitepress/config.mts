94717792306bbd893dfd583ecc41c213abe6ec89
stydxm <stydxm@outlook.com>
2024-08-24 08:46:54 +0800

    还原提交，解决使用window导致SSR报错
    This reverts commit c929929ede9e6b1d2bbcea18c44d6bce8f182268.

--- .vitepress/config.mts
+++ .vitepress/config.mts
@@ -11,7 +11,8 @@ export default withMermaid({
     ['meta', { property: 'og:type', href: 'website' }],
     ['meta', { property: 'og:title', href: '浙理钱塘蛟' }],
     ['meta', { property: 'og:description', href: '浙江理工大学 Robomaster 战队' }],
-    ['meta', { property: 'og:img', href: 'https://docs.015609.best/logo.png' }]],
+    ['meta', { property: 'og:img', href: 'https://docs.015609.best/logo.png' }],
+    ['script', { async: '', src: "/global.js" }]],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -10,30 +10,3 @@ export default {
         app.component('Qrcode', Qrcode);
     }
 }
-
-const ua = window.navigator.userAgent.toLowerCase()
-
-function detectBrowser() {
-    if (ua.includes("micromessenger")) {
-        return "微信"
-    } else if (ua.includes(" qq") && !ua.includes("mqqbroser")) {
-        return "QQ"
-    }
-    else {
-        return
-    }
-}
-
-function detectMobile() {
-    return ua.includes("mobi") || ua.includes("android") || ua.includes("iphone")
-}
-
-const dir = window.location.href.toString().split(window.location.host)[1]
-const browser = detectBrowser()
-const mobile = detectMobile()
-if (dir !== "/") {
-    if (browser) alert(`请不要使用${browser}访问\n建议使用电脑端的chrome或firefox或edge`)
-    else if (mobile) alert("建议使用电脑访问")
-} else {
-    alert(dir)
-}
