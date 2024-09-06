e85aed780895120ae8782670b41bb58025f6c557
stydxm <stydxm@outlook.com>
2024-09-06 14:00:43 +0800

    去除手机访问的弹窗

--- public/global.js
+++ public/global.js
@@ -20,5 +20,5 @@ const browser = detectBrowser()
 const mobile = detectMobile()
 if (dir !== "/") {
     if (browser) alert(`请不要使用${browser}访问\n建议使用电脑端的chrome或firefox或edge`)
-    else if (mobile) alert("建议使用电脑访问")
+    // else if (mobile) alert("建议使用电脑访问")
 }
