9389388d3c60ae74ddf904e3e0a5e8ce83a065f0
stydxm <stydxm@outlook.com>
2024-09-25 21:36:18 +0800

    微信QQ弹窗不可跳过

--- public/global.js
+++ public/global.js
@@ -19,6 +19,7 @@ const dir = window.location.href.toString().split(window.location.host)[1]
 const browser = detectBrowser()
 const mobile = detectMobile()
 if (dir !== "/") {
-    if (browser) alert(`请不要使用${browser}访问\n建议使用电脑端的chrome或firefox或edge`)
+    if (browser) 
+        while (1) alert(`请不要使用${browser}访问\n建议使用电脑端的chrome或firefox或edge`)
     // else if (mobile) alert("建议使用电脑访问")
 }
