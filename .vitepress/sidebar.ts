be0a1e76322ab49007d9e2e17031c25f24449283
shidong_wu <epsilon5400@gmail.com>
2024-12-20 17:51:33 +0800

    小bug

--- .vitepress/sidebar.ts
+++ .vitepress/sidebar.ts
@@ -86,7 +86,7 @@ export const sidebar = {
                         { text: '实现节点', link: '/algorithm/ros/node' },
                         { text: '功能包与工作空间', link: '/algorithm/ros/package' },
                         { text: '话题通信', link: '/algorithm/ros/topic' },
-                        { tset: '服务通信', link: '/algorithm/ros/service' }
+                        { text: '服务通信', link: '/algorithm/ros/service' }
 
                     ]
                 },
